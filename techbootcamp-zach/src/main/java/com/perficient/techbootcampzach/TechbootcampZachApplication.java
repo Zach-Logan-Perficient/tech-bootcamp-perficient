0b90e7e5dec31f9610b26200091ee4118a919022
ZachL <zach.logan@perficient.com>
2022-06-13 17:25:42 -0500

    Initial Project

---
+++ techbootcamp-zach/src/main/java/com/perficient/techbootcampzach/TechbootcampZachApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampzach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbootcampZachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbootcampZachApplication.class, args);
+	}
+
+}
