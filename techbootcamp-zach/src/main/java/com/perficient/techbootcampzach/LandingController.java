e2a5c323a021858385f40a7afa3adf209c56b757
ZachL <zach.logan@perficient.com>
2022-06-14 16:38:32 -0500

    Basic Hi page

---
+++ techbootcamp-zach/src/main/java/com/perficient/techbootcampzach/LandingController.java
@@ -0,0 +1,25 @@
+package com.perficient.techbootcampzach;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller
+public class LandingController {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private final Date dateNow = new Date();
+	
+    @RequestMapping("/")
+    @ResponseBody
+    public String goToLanding () {
+    	
+        logger.error("error at {}??", dateNow.toString());
+        logger.info("no, there is no error right now!");
+        return "<h1>Hi!</h1>";
+    }
+}
